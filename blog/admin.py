d06ec7a8e47225048feb74af5de78d42a30a0da3
masoudsajjad <masoud67130@gmail.com>
2020-08-18 18:35:39 +0430

    requiremets file

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(UserProfile)
-admin.site.register(Article)
-admin.site.register(Category)
+
+class AdminUserProfile(admin.ModelAdmin):
+    list_display = ['user', 'avatar', 'description']
+
+
+admin.site.register(UserProfile, AdminUserProfile)
+
+
+class AdminArticle(admin.ModelAdmin):
+    search_fields = ['title', 'content']
+    list_display = ['title', 'created_at', 'category', 'author']
+
+
+admin.site.register(Article, AdminArticle)
+
+
+class AdminCategory(admin.ModelAdmin):
+    list_display = ['title', 'cover']
+
+
+admin.site.register(Category, AdminCategory)
 
