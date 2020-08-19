2a66241c3e39919bd1ff2107a754584f0a44f46d
masoudsajjad <masoud67130@gmail.com>
2020-08-20 01:19:42 +0430

    contact page

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.IndexPage.as_view(), name='Index')
+    url(r'^$', views.IndexPage.as_view(), name='Index'),
+    url(r'^contact/$', views.ContactPage.as_view(), name='contact')
 ]
--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,6 @@ class IndexPage(TemplateView):
 
         article_data = []
         all_articles = Article.objects.all()[:9]
-
         for article in all_articles:
             article_data.append({
                 'title': article.title,
@@ -18,8 +17,28 @@ class IndexPage(TemplateView):
                 'created_at': article.created_at,
 
             })
+
+        promote_data = []
+        all_promote_article = Article.objects.filter(promote=True)
+        for promote_article in all_promote_article:
+            promote_data.append({
+                'title': promote_article.title,
+                'cover': promote_article.cover.url,
+                'category': promote_article.category.title,
+                'author': promote_article.author.user.first_name,
+                'avatar': promote_article.author.avatar.url if promote_article.author.avatar else None,
+                'created_at': promote_article.created_at,
+
+            })
+
         context = {
             'article_data': article_data,
+            'promote_data_article': promote_data,
         }
 
         return render(request, 'index.html', context)
+
+
+class ContactPage(TemplateView):
+    template_name = "page-contact.html"
+
