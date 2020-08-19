569e4bd1b3c81cd22de94d2a7d5a4a9d7d93e0be
masoudsajjad <masoud67130@gmail.com>
2020-08-19 18:54:31 +0430

    index.html

--- blogpy/urls.py
+++ blogpy/urls.py
@@ -9,7 +9,7 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
+Including anot her URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
@@ -20,4 +20,6 @@ from django.conf.urls import url, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
+    url(r'^', include('blog.urls'))
+
 ]
